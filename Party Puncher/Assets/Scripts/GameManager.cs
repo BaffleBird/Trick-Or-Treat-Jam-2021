6ff4bcaee27bb12b85dcce2bd6af191c4943e9e1
lamcal <lamcal@oregonstate.edu>
2021-10-20 22:59:21 -0500

    Singleton implementation
    Implement singleton class to allow manager classes to inherit from.

--- Party Puncher/Assets/Scripts/GameManager.cs
+++ Party Puncher/Assets/Scripts/GameManager.cs
@@ -3,18 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    //Set of scene names
+    // Prevent non-singleton constructor use
+    protected GameManager() { }
+
+    // Add whatever code to the class you need as you normally would
+    public string MyTestString = "Hello world!";
+
+    // Set of scene names
     public enum Scene
     {
         MainMenuScene,
         GameScene
     }
 
-    //Load function for game scenes
+    // Load function for game scenes
     public static void Load(Scene scene)
     {
         SceneManager.LoadScene(scene.ToString());
+    }
+
+    public void getNumber()
+    {
+        return;
+    }
+}
+
+
+public class Testing : MonoBehaviour
+{
+    // test
+    private void OnEnable()
+    {
+        Debug.Log(GameManager.Instance.MyTestString);
     }
 }
