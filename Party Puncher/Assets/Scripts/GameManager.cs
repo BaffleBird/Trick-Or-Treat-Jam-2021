54a4f4435fc0d59fa99bb7b02882e98e9e878e0b
lamcal <lamcal@oregonstate.edu>
2021-10-27 19:03:49 -0500

    Update GameManager
    Removed AudioManager overlap

--- Party Puncher/Assets/Scripts/GameManager.cs
+++ Party Puncher/Assets/Scripts/GameManager.cs
@@ -6,63 +6,14 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    AudioSource audioSource;
-
-    string currentScene = "MainMenu";
 
     void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
-        // Initial menu music can be loaded at start
-        // Use update to check scene for corresponding scene music
-        audioSource.clip = Resources.Load<AudioClip>("Music/TipToes");
-
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-
-        // Initial play for first time Main Menu scene
-        audioSource.Play();
-    }
-
-    void Update()
-    {
-        if (currentScene != SceneManager.GetActiveScene().name)
-        {
-            currentScene = SceneManager.GetActiveScene().name;
-            SwitchAudioClip(SceneManager.GetActiveScene().name);
-        }
-    }
-
-    // Assign AudioClip to specified scene, called via Update whenever scene change is detected
-    public void SwitchAudioClip (string targetScene)
-    {
-        if (targetScene == "TestMusic1")
-        {
-            audioSource.clip = Resources.Load<AudioClip>("Music/TipToes");
-        }
-        else if (targetScene == "TestMusic2")
-        {
-            audioSource.clip = Resources.Load<AudioClip>("Music/TipToes");
-        }
-        else if (targetScene == "GameScene")
-        {
-            audioSource.clip = Resources.Load<AudioClip>("Music/Swing");
-            // Serves as 'Play On Awake' for transition from Menu music to Game music
-            audioSource.Play();
-        }
-    }
-
-    public void PlaySound()
-    {
-        audioSource.Play();
-    }
-
-    public void PauseSound()
-    {
-        audioSource.Pause();
     }
 
     // Set of scene names
