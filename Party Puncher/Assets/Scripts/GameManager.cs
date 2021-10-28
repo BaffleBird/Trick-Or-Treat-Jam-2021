2e5d3e9fa892b84038157b97750bef094f43d515
lamcal <lamcal@oregonstate.edu>
2021-10-27 19:30:47 -0500

    Merge conflict adjust

--- Party Puncher/Assets/Scripts/GameManager.cs
+++ Party Puncher/Assets/Scripts/GameManager.cs
@@ -5,28 +5,69 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    //Systems Instantiations
+    public DataSystem dataSystem = new DataSystem();
 
-    void Awake()
+    //Unity Singleton
+    #region Singleton
+    //Basically only one of this class can exist across scenes. If you need something from it, call GameManager.instance.whatever
+    private static GameManager _instance;
+    public static GameManager instance => _instance; // Global Reference
+
+    private void Awake()
     {
-        if (Instance == null)
+        //Destroy duplicates
+        if (_instance != null && _instance != this)
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        //Preserve across scenes
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void Update()
+    {
+        dataSystem.Update();
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    //Things to do at after scene loads
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "GameScene")
+        {
+            dataSystem.Reset();
         }
     }
 
-    // Set of scene names
-    public enum Scene
+    //Set of scene names
+    public enum Scenes
     {
-        MainMenu,
+        MainMenuScene,
         GameScene,
         ScoreScene
     }
 
-    // Load function for game scenes
-    public static void Load(Scene scene)
+    //Load function for game scenes
+    public static void Load(Scenes scene)
     {
         SceneManager.LoadScene(scene.ToString());
     }
+
+
 }
